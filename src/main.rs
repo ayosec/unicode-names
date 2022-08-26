79718bb0c5ef66dfc87160a3222aa2f3e257b412
Ayose <ayosec@gmail.com>
2022-08-26 19:26:11 +0100

    Filter Unicode characters using patters from the command-line.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,21 @@
 //!
 //! Search Unicode characters using substrings against their names.
 
+use std::env;
+
 mod charstable;
 
-fn main() {}
+fn main() {
+    let patterns: Vec<_> = env::args().skip(1).map(|arg| arg.to_uppercase()).collect();
+
+    if patterns.is_empty() {
+        eprintln!("Usage: unicode-names [patterns]+");
+        return;
+    }
+
+    for (chr, name) in charstable::Table::new() {
+        if patterns.iter().all(|pat| name.contains(pat)) {
+            println!(" {chr:}\tU+{:<6X} {name}", chr as u32);
+        }
+    }
+}
